c137668391b813d5f5dd49cb0acc022c98463e95
jyochsna <55848802+jyochsna@users.noreply.github.com>
2020-01-14 16:18:28 -0800

    js code added

---
+++ assets/javascript/app.js
@@ -0,0 +1,33 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBpAFfSOMwnqusvo4EDKqbTMgkbsEJI40Y",
+    authDomain: "train-schedule-ec30e.firebaseapp.com",
+    databaseURL: "https://train-schedule-ec30e.firebaseio.com",
+    projectId: "train-schedule-ec30e",
+    storageBucket: "train-schedule-ec30e.appspot.com",
+    messagingSenderId: "797569406143",
+    appId: "1:797569406143:web:ed7f00036439f40fc5e20a",
+    measurementId: "G-QP30MVR9NR"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  var dataRef = firebase.database();
+
+  $("#submit").on("click", function(event){
+      event.preventDefault();
+
+      var trainName = $("train-name").val().trim();
+      var destination = $("#destination").val().trim();
+      var firstTime = $("first-time").val().trim();
+      var frequency = $("#frequency").val().trim();
+
+      daraRef.ref().push({
+    
+        trainName,
+        destination,
+        firstTime,
+        frequency,
+
+      })
+  })
